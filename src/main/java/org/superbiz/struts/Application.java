357a9562d515305785c851739b2eb3d88af5e58c
mstnreddy <mstnreddy@gmail.com>
2018-12-10 16:08:30 -0700

    struts spring boot changes added final version

---
+++ src/main/java/org/superbiz/struts/Application.java
@@ -0,0 +1,55 @@
+package org.superbiz.struts;
+
+import org.apache.struts2.dispatcher.FilterDispatcher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+   /* @Bean
+    public FilterDispatcher struts2() {
+        return new FilterDispatcher(actionServlet, "/moviefun/*");
+    }*/
+
+
+    @Bean
+    public FilterRegistrationBean struts2() {
+        FilterRegistrationBean filterRegistrationBean= new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new org.apache.struts2.dispatcher.FilterDispatcher());
+        Map<String,String> initparamMap=new HashMap<String,String>();
+        initparamMap.put("actionPackages","com.lq");
+        filterRegistrationBean.setInitParameters(initparamMap);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean struts_cleanup() {
+        FilterRegistrationBean filterRegistrationBean= new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new org.apache.struts2.dispatcher.ActionContextCleanUp());
+
+        return filterRegistrationBean;
+    }
+
+
+    @Bean
+    public FilterRegistrationBean sitemesh() {
+        FilterRegistrationBean filterRegistrationBean= new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new com.opensymphony.module.sitemesh.filter.PageFilter());
+
+        return filterRegistrationBean;
+    }
+
+
+
+
+}
--- src/main/java/org/superbiz/struts/FindUser.java
+++ src/main/java/org/superbiz/struts/FindUser.java
@@ -17,11 +17,21 @@
  */
 package org.superbiz.struts;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import java.util.Properties;
 
+@Component
 public class FindUser {
+
+    private  UserService userService;
+
+    public FindUser(UserService userService) {
+        this.userService = userService;
+    }
 
     private int id;
     private String errorMessage;
@@ -54,13 +64,8 @@ public class FindUser {
     public String execute() {
 
         try {
-            UserService service = null;
-            Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY,
-                "org.apache.openejb.core.LocalInitialContextFactory");
-            Context ctx = new InitialContext(props);
-            service = (UserService) ctx.lookup("UserServiceImplLocal");
-            this.user = service.find(id);
+
+            this.user = userService.find(id);
         } catch (Exception e) {
             this.errorMessage = e.getMessage();
             return "failure";
