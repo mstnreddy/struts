0c77b17df4ae9f22041b822ed39cc60335e6263d
mstnreddy <mstnreddy@gmail.com>
2018-12-11 08:33:56 -0700

    struts final changes

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -25,6 +25,7 @@ public class Application {
     @Bean
     public FilterRegistrationBean struts2() {
         FilterRegistrationBean filterRegistrationBean= new FilterRegistrationBean();
+
         filterRegistrationBean.setFilter(new org.apache.struts2.dispatcher.FilterDispatcher());
         Map<String,String> initparamMap=new HashMap<String,String>();
         initparamMap.put("actionPackages","com.lq");
@@ -34,7 +35,9 @@ public class Application {
 
     @Bean
     public FilterRegistrationBean struts_cleanup() {
+
         FilterRegistrationBean filterRegistrationBean= new FilterRegistrationBean();
+
         filterRegistrationBean.setFilter(new org.apache.struts2.dispatcher.ActionContextCleanUp());
 
         return filterRegistrationBean;
